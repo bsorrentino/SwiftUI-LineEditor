057ede87c9fde1e30427434b03ecd95a3ca0f64b
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-02-13 21:19:12 +0100

    feat(SyntaxTextField): allow remove token

--- SyntaxTextField.playground/Contents.swift
+++ SyntaxTextField.playground/Contents.swift
@@ -12,17 +12,43 @@ struct SyntaxTextData:  CustomStringConvertible {
     var description:String {
         (token.isEmpty) ? trailingText : "\(token) \(trailingText)"
     }
+    
 
 }
 
 class SyntaxTextObject : ObservableObject {
     var textElements: Array<SyntaxTextData> = []
+
+    
+
+    func setTrailingText( _ text: String, at index: Int ) {
+        guard index < textElements.endIndex else { return }
+        textElements[ index ].trailingText = text
+    }
+
+    func getTrailingToken( at index: Int ) -> String? {
+        guard index < textElements.endIndex else { return nil  }
+        return textElements[ index ].token
+    }
+
+    func removeElement( at index: Int ) {
+        guard index < textElements.endIndex else { return }
+        textElements.remove(at: index)
+    }
     
     func evaluate() -> String {
         textElements
             .reduce("") { partialResult, data in
                     partialResult + "\(data) "
             }
+        
+    }
+
+    let keywords = /(?i)^\s*(usecase|actor|object|participant|boundary|control|entity|database|create|component|interface|package|node|folder|frame|cloud|annotation|enum|abstract|class|abstract\s+class|state|autonumber(\s+stop|resume)?|activate|deactivate|destroy|newpage|alt|else|opt|loop|par|break|critical|group|box|rectangle|namespace|partition|archimate|sprite|left|right|side|top|bottom)\b/
+
+    func match( with text: String ) -> Bool {
+        
+        return text.firstMatch(of: keywords) != nil
         
     }
     
@@ -34,7 +60,9 @@ class SyntaxTextObject : ObservableObject {
             .map { _, element in
                 SyntaxTextData( token: element, trailingText: "" )
         }
-        
+        if textElements.isEmpty {
+            textElements = [ SyntaxTextData( token: "", trailingText: "" )]
+        }
         return textElements.indices
     }
 }
@@ -47,18 +75,34 @@ struct SyntaxTextView : View {
 
     var body: some View {
         HStack {
-            Text( syntaxTextObject.textElements[index].token )
-                .padding( 7 )
-                .background(.red)
-//                .cornerRadius(15)
-                .clipShape(Capsule())
+            let token = syntaxTextObject.getTrailingToken( at: index )
+            if let token, !token.isEmpty {
+                ZStack(alignment: .trailing) {
+                    Text( token )
+                        .padding( EdgeInsets(top: 7, leading: 7, bottom: 7, trailing: 18))
+                        .background(.red)
+        //                .cornerRadius(15)
+                        .clipShape(Capsule())
+                    
+                    Button( action: {
+                        syntaxTextObject.removeElement(at: index)
+                        syntaxTextObject.objectWillChange.send()
+                    }) {
+                        Image( systemName: "x.circle")
+                            .resizable()
+                            .frame( width: 13, height: 13)
+                    }
+                    .padding( .trailing, 3 )
+                    
+                }
+            }
             TextField( "", text: $text , onEditingChanged: { editing in
                 if editing {
-                    syntaxTextObject.textElements[index].trailingText = text
+                    syntaxTextObject.setTrailingText( text, at: index )
                 }
                 
             }, onCommit: {
-                syntaxTextObject.textElements[index].trailingText = text
+                syntaxTextObject.setTrailingText( text, at: index )
                 syntaxTextObject.objectWillChange.send()
 
             })
@@ -102,13 +146,14 @@ struct SyntaxTextField : View {
 
 
 struct ContentView: View {
-    @State var text = "participant Test as p1"
+    @State var text = "token1 token2 token3"
     var body: some View {
         SyntaxTextField( text: $text )
+            .border(.red)
             .onChange(of: text ) { newValue in
                 print( "onChange: \(newValue)" )
             }
-            .frame(minWidth: 1000, alignment: .center)
+            .frame(minWidth: 500, alignment: .center)
     }
 }
 
