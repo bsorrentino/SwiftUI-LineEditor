5c7b5acc964ac7deb17fdafad5d41c1bed7b10d8
Build Pipeline <build.pipeline@softphone.it>
2023-02-13 18:34:48 +0100

    feat(SyntaxTextField): playground impl refinement

--- SyntaxTextField.playground/Contents.swift
+++ SyntaxTextField.playground/Contents.swift
@@ -4,34 +4,95 @@ import SwiftUI
 import PlaygroundSupport
 
 
-class SyntaxText : ObservableObject {
-    @Published var textElements: Array<String> = []
+struct SyntaxTextData:  CustomStringConvertible {
     
-    func tokens( text: String ) -> Array<String> {
-        text.components(separatedBy: " ")
+    var token: String = ""
+    var trailingText:String = ""
+    
+    var description:String {
+        (token.isEmpty) ? trailingText : "\(token) \(trailingText)"
+    }
+
+}
+
+class SyntaxTextObject : ObservableObject {
+    var textElements: Array<SyntaxTextData> = []
+    
+    func evaluate() -> String {
+        textElements
+            .reduce("") { partialResult, data in
+                    partialResult + "\(data) "
+            }
+        
+    }
+    
+    func tokens( from text: String ) -> Range<Int> {
+        
+        textElements = text.components(separatedBy: " ")
+            .enumerated()
+            .filter { _, element in !element.isEmpty }
+            .map { _, element in
+                SyntaxTextData( token: element, trailingText: "" )
+        }
+        
+        return textElements.indices
     }
 }
 
+struct SyntaxTextView : View {
+    @EnvironmentObject private var syntaxTextObject: SyntaxTextObject
+
+    var index: Int
+    @State var text: String = ""
+
+    var body: some View {
+        HStack {
+            Text( syntaxTextObject.textElements[index].token )
+                .padding( 7 )
+                .background(.red)
+//                .cornerRadius(15)
+                .clipShape(Capsule())
+            TextField( "", text: $text , onEditingChanged: { editing in
+                if editing {
+                    syntaxTextObject.textElements[index].trailingText = text
+                }
+                
+            }, onCommit: {
+                syntaxTextObject.textElements[index].trailingText = text
+                syntaxTextObject.objectWillChange.send()
+
+            })
+        }
+    }
+}
 
 struct SyntaxTextField : View {
     
     @Binding var text: String
-    @StateObject var syntaxText  = SyntaxText()
-    @State private var trailingText: String = ""
-    @State private var leadingText: String = ""
+    @StateObject var syntaxTextObject  = SyntaxTextObject()
+    @State private var theId = 0
+
     var body: some View {
         
         ScrollView( .horizontal, showsIndicators: false) {
             HStack(spacing: 5) {
-                TextField( "", text: $leadingText )
-                ForEach( text.components(separatedBy: " "), id: \.self ) {
-                    Text( $0)
-                        .padding( 7 )
-                        .background(.red)
-                    //.cornerRadius(15)
-                        .clipShape(Capsule())
+                
+                ForEach( syntaxTextObject.tokens( from: text ), id: \.self ) { index in
+                    
+                    SyntaxTextView(index: index )
+                        .id(index)
+                        .environmentObject(syntaxTextObject)
                 }
-                TextField( "", text: $trailingText )
+                .id( theId )
+            }
+            .onReceive(syntaxTextObject.objectWillChange) { _ in
+                
+                print( "syntaxTextObject changed")
+                let newValue = syntaxTextObject.evaluate()
+                print( "syntaxTextObject changed [\(newValue)]")
+
+                text = newValue
+                theId += 1
             }
         }
     }
@@ -44,6 +105,10 @@ struct ContentView: View {
     @State var text = "participant Test as p1"
     var body: some View {
         SyntaxTextField( text: $text )
+            .onChange(of: text ) { newValue in
+                print( "onChange: \(newValue)" )
+            }
+            .frame(minWidth: 1000, alignment: .center)
     }
 }
 
