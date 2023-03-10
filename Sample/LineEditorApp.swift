de73759f4b5560685fe582bacb1c83d86e2ab211
Build Pipeline <build.pipeline@softphone.it>
2023-03-10 17:32:34 +0100

    test(LineEditorApp): update test app

--- Sample/LineEditorApp.swift
+++ Sample/LineEditorApp.swift
@@ -12,19 +12,23 @@ import LineEditor
 struct LineEditorApp: App {
     let line_begin_keywords = "(?i)^\\s*(usecase|actor|object|participant|boundary|control|entity|database|create|component|interface|package|node|folder|frame|cloud|annotation|class|state|autonumber|group|box|rectangle|namespace|partition|archimate|sprite)\\b"
 
+    @State private var selectedTab = "syntax"
+    
     var body: some Scene {
         WindowGroup {
-            TabView {
+            TabView(selection: $selectedTab) {
                
                 StandardEditorView()
                     .tabItem {
                         Label("Standard Line Editor", image: "")
                     }
+                    .tag("standard")
 
                 SyntaxEditorView()
                     .tabItem {
                         Label("Syntax Line Editor", image: "")
                     }
+                    .tag("syntax")
 
                 VStack {
                     Spacer( minLength: 200)
@@ -40,6 +44,7 @@ struct LineEditorApp: App {
                 .tabItem {
                     Label("SyntaxTextField", image: "")
                 }
+                .tag("test")
             }
         }
     }
